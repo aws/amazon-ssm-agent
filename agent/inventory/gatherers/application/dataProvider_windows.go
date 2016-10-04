c5e05caf4a3fdbb087d4a1e31fb9752391851865
Saurabh Shankar <fshankar@amazon.com>
2016-10-04 13:02:22 -0700

    adding windows implementation of dataProvider for aws:application gatherer
    cr https://cr.amazon.com/r/6021874/

--- agent/inventory/gatherers/application/dataProvider_windows.go
+++ agent/inventory/gatherers/application/dataProvider_windows.go
@@ -18,53 +18,149 @@
 package application
 
 import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"os/exec"
+	"strings"
+
 	"github.com/aws/amazon-ssm-agent/agent/context"
 	"github.com/aws/amazon-ssm-agent/agent/inventory/model"
 )
 
 // TODO: add unit tests
 
+const (
+	PowershellCmd            = "powershell"
+	ArgsFor32BitApplications = `Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\* | Where-Object {$_.DisplayName -ne $null} | Select-Object @{Name="Name";Expression={$_."DisplayName"}}, @{Name="Version";Expression={$_."DisplayVersion"}}, Publisher, @{Name="InstalledTime";Expression={$_."InstallDate"}} | ConvertTo-Json`
+	ArgsFor64BitApplications = `Get-ItemProperty HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* | Where-Object {$_.DisplayName -ne $null} | Select-Object @{Name="Name";Expression={$_."DisplayName"}}, @{Name="Version";Expression={$_."DisplayVersion"}}, Publisher, @{Name="InstalledTime";Expression={$_."InstallDate"}} | ConvertTo-Json`
+
+	Arch64Bit = "64-Bit"
+	Arch32Bit = "32-Bit"
+)
+
 // CollectApplicationData collects application data for windows platform
 func CollectApplicationData(context context.T) []inventory.ApplicationData {
-	//implementation is missing
-	return CreateMultipleApplicationEntries()
-}
 
-// CreateMultipleApplicationEntries generates fake data with 2 entries for aws:application data
-func CreateMultipleApplicationEntries() []inventory.ApplicationData {
-	var data []inventory.ApplicationData
+	/*
+		Note:
 
-	//visual studio
-	visualStudio := inventory.ApplicationData{
-		Name:          "Visual Studio 10",
-		Publisher:     "Microsoft Corporation",
-		Version:       "14.1.0.1985",
-		InstalledTime: "2016-01-01T09:10:10Z",
-	}
+		We use powershell to query registry for a list of 64 bit windows apps using following command:
 
-	//adobe reader 10
-	adobeReader := inventory.ApplicationData{
-		Name:          "Adobe Reader 10",
-		Publisher:     "Adobe",
-		Version:       "10.10.1.3",
-		InstalledTime: "2016-01-01T09:10:10Z",
-	}
+		Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\* | Where-Object {$_.DisplayName -ne $null} | Select-Object @{Name="Name";Expression={$_."DisplayName"}} | ConvertTo-Json
 
-	data = append(data, visualStudio)
-	data = append(data, adobeReader)
+		Similarly we use following command to query list of 32 bit windows apps:
+
+		Get-ItemProperty HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* | Where-Object {$_.DisplayName -ne $null} | Select-Object @{Name="Name";Expression={$_."DisplayName"}} | ConvertTo-Json
+
+		We make use of Calculated property of Select-Object to format the data accordingly.
+
+		For more details refer to: https://technet.microsoft.com/en-us/library/ff730948.aspx
+	*/
+
+	//TODO: Verify if HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\* contains 32 bit applications for 32 bit OS.
+	//TODO: powershell commands can be put in a script to generate that data - and then we can simply execute the script to get the data.
+	//it will enable us to run other complicated queries too.
+
+	var data, apps []inventory.ApplicationData
+
+	//getting all 64 bit applications
+	apps = ExecutePowershellCommands(context, PowershellCmd, ArgsFor64BitApplications, Arch64Bit)
+	data = append(data, apps...)
+
+	//getting all 32 bit applications
+	apps = ExecutePowershellCommands(context, PowershellCmd, ArgsFor32BitApplications, Arch32Bit)
+	data = append(data, apps...)
 
 	return data
 }
 
-// CreateSingleApplicationEntry generates fake data with 1 entry for aws:application data
-func CreateSingleApplicationEntry() inventory.ApplicationData {
-	//visual studio
-	visualStudio := inventory.ApplicationData{
-		Name:          "Visual Studio 10",
-		Publisher:     "Microsoft Corporation",
-		Version:       "14.1.0.1985",
-		InstalledTime: "2016-01-01T09:10:10Z",
+// ExecutePowershellCommands executes commands in powershell to get all windows applications installed.
+func ExecutePowershellCommands(context context.T, command, args, arch string) (data []inventory.ApplicationData) {
+
+	log := context.Log()
+	var out bytes.Buffer
+	var e bytes.Buffer
+	var err error
+
+	cmd := exec.Command(command, args)
+	cmd.Stdout = &out
+	cmd.Stderr = &e
+
+	log.Infof("Getting all %v windows applications", arch)
+	log.Infof("Executing command: %v", cmd.Args)
+
+	if err = cmd.Run(); err != nil {
+		log.Debugf("Failed to execute command : %v with error - %v", cmd.Args, err.Error())
+		log.Debugf("Command Stderr: %v", e.String())
+		err = errors.New(fmt.Sprintf("Command failed with error: %v", e.String()))
+	} else {
+		cmdOutput := out.String()
+		log.Debugf("Command output: %v", cmdOutput)
+
+		if data, err = ConvertToApplicationData(cmdOutput, arch); err != nil {
+			log.Errorf("Unable to convert query output to ApplicationData - %v", err.Error())
+		} else {
+			log.Infof("Number of %v applications detected by %v - %v", arch, GathererName, len(data))
+
+			str, _ := json.Marshal(data)
+			log.Debugf("Gathered applications: %v", string(str))
+		}
 	}
 
-	return visualStudio
+	return
+}
+
+// ConvertToApplicationData converts powershell command output to an array of inventory.ApplicationData
+func ConvertToApplicationData(cmdOutput, architecture string) (data []inventory.ApplicationData, err error) {
+	//This implementation is closely tied to the kind of powershell command we run in windows. A change in command
+	//MUST be accompanied with a change in json conversion logic as well.
+
+	/*
+			Sample powershell command that we run in windows to get applications information:
+
+			Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\* |
+			Where-Object {$_.DisplayName -ne $null} |
+			Select-Object @{Name="Name";Expression={$_."DisplayName"}},@{Name="Version";Expression={$_."DisplayVersion"}} |
+			ConvertTo-Json
+
+			Above command will generate data in json format:
+			[
+			    {
+				"Name":  "EC2ConfigService",
+				"Version":  "3.17.1032.0"
+			    },
+			    {
+				"Name":  "aws-cfn-bootstrap",
+				"Version":  "1.4.10"
+			    },
+			    {
+				"Name":  "AWS PV Drivers",
+				"Version":  "7.3.2"
+			    }
+			]
+
+		        Since command output is in json - we do following operations:
+		        - trim spaces
+		        - unmarshal the string
+		        - add architecture details as given input
+
+	*/
+
+	//trim spaces
+	str := strings.TrimSpace(cmdOutput)
+
+	//unmarshall json string & add architecture information
+	if err = json.Unmarshal([]byte(str), &data); err == nil {
+
+		//iterate over all entries and add default value of architecture as given input
+		for i, v := range data {
+			//set architecture to given input
+			v.Architecture = architecture
+			data[i] = v
+		}
+	}
+
+	return
 }
