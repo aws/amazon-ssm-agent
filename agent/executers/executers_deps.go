c818d1fc98d914fe40e608f27c556cb34f51947a
Ford <mattfo@amazon.com>
2016-10-06 11:11:16 -0700

    Updates based on review feedback

--- agent/executers/executers_deps.go
+++ agent/executers/executers_deps.go
@@ -1,16 +1,18 @@
 // Copyright 2016 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 //
-// Licensed under the Amazon Software License (the "License"). You may not
+// Licensed under the Apache License, Version 2.0 (the "License"). You may not
 // use this file except in compliance with the License. A copy of the
 // License is located at
 //
-// http://aws.amazon.com/asl/
+// http://aws.amazon.com/apache2.0/
 //
 // or in the "license" file accompanying this file. This file is distributed
 // on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
-// express or implied. See the License for the specific language governing
+// either express or implied. See the License for the specific language governing
 // permissions and limitations under the License.
 
+// Package executers contains general purpose (shell) command executing objects.
+// executers_deps contains platform dependencies that should be mockable in tests
 package executers
 
 import (
