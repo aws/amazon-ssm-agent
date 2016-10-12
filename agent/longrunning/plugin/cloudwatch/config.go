3408f616816cb527e4a27b6f995599f559b4d7ff
Chan Jeon <chanjeon@amazon.com>
2016-10-11 18:22:06 -0700

    Fixed the escape character issue with cloudwatch plugin
    cr https://cr.amazon.com/r/6059423/

--- agent/longrunning/plugin/cloudwatch/config.go
+++ agent/longrunning/plugin/cloudwatch/config.go
@@ -19,6 +19,7 @@ import (
 	"os"
 	"sync"
 
+	"encoding/json"
 	"github.com/aws/amazon-ssm-agent/agent/appconfig"
 	"github.com/aws/amazon-ssm-agent/agent/fileutil"
 	"github.com/aws/amazon-ssm-agent/agent/jsonutil"
