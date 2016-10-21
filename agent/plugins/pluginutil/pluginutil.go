3e441a89971165ed802dbd53e96fcc6f231a5d24
Chan Jeon <chanjeon@amazon.com>
2016-10-21 14:48:03 -0700

    Fixed SSMAgent to execute the plugin again if reboot occured by request
    cr https://cr.amazon.com/r/6123965/

--- agent/plugins/pluginutil/pluginutil.go
+++ agent/plugins/pluginutil/pluginutil.go
@@ -365,7 +365,13 @@ func PersistPluginInformationToCurrent(log log.T, pluginName string, config cont
 	//set plugin state's execution details
 	pluginState.Configuration = config
 	pluginState.Result = res
-	pluginState.HasExecuted = true
+
+	//set HasExecuted based on result status.
+	if pluginState.Result.Status == contracts.ResultStatusSuccessAndReboot {
+		pluginState.HasExecuted = false
+	} else {
+		pluginState.HasExecuted = true
+	}
 
 	command_state_helper.PersistPluginState(log,
 		pluginState,
