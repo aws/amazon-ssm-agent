fc3b84797f8261b9eaa0fc4b237231e158c7d08d
Ford <mattfo@amazon.com>
2016-10-26 13:09:39 -0700

    Integration test updates

--- agent/plugins/configurepackage/configurepackage_integ_test.go
+++ agent/plugins/configurepackage/configurepackage_integ_test.go
@@ -11,6 +11,8 @@
 // either express or implied. See the License for the specific language governing
 // permissions and limitations under the License.
 
+// +build integration
+
 // Package configurepackage implements the ConfigurePackage plugin.
 package configurepackage
 
