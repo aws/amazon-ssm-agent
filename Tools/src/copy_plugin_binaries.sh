f662a894a398ab4b29bcb0bcb124500d68d24069
Shihua Zhang <shihua@amazon.com>
2017-01-25 00:52:40 +0000

    Add DomainJoin and CloudWatch release bits under the packaging folder, fixed the copy_plugin_binaries.sh to copy files from Packaging folder for non Brazil build
    cr https://cr.amazon.com/r/6567946/

--- Tools/src/copy_plugin_binaries.sh
+++ Tools/src/copy_plugin_binaries.sh
@@ -4,7 +4,16 @@ echo "Copying DomainJoin and CloudWatch binaries"
 echo "****************************************"
 
 BIN_FOLDER="$BGO_SPACE/bin/"
-PLUGIN_BINARIES="`brazil-path run.configfarm.artifacts`/artifacts"
+
+brazil_build=$1
+
+if [[ $brazil_build = true ]]; then
+    PLUGIN_BINARIES="`brazil-path run.configfarm.artifacts`/artifacts"
+else
+    PLUGIN_BINARIES="$BGO_SPACE/packaging/dependencies"
+fi
+
+echo "Plugin binaries location is "$PLUGIN_BINARIES
 
 cp $PLUGIN_BINARIES/AWS.CloudWatch.exe $BIN_FOLDER
 cp $PLUGIN_BINARIES/AWS.CloudWatch.exe.config $BIN_FOLDER
