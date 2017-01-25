26c510fcb1b9c72ffd6e54b9cc0abd8c763fb533
Shihua Zhang <shihua@amazon.com>
2017-01-25 21:07:04 +0000

    Add script and makefile target for update/release plugin binaries
    cr https://cr.amazon.com/r/6573988/

---
+++ Tools/src/release_dependencies.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+echo "********************************************"
+echo "Release  Binary Dependencies for Open Source"
+echo "********************************************"
+
+PACKAGING_FOLDER="$BGO_SPACE/packaging/dependencies/"
+PLUGIN_BINARIES="`brazil-path run.configfarm.artifacts`/artifacts"
+
+cp $PLUGIN_BINARIES/AWS.CloudWatch.exe $PACKAGING_FOLDER
+cp $PLUGIN_BINARIES/AWS.CloudWatch.exe.config $PACKAGING_FOLDER
+cp $PLUGIN_BINARIES/AWS.DomainJoin.exe $PACKAGING_FOLDER
+cp $PLUGIN_BINARIES/AWS.DomainJoin.exe.config $PACKAGING_FOLDER
+cp $PLUGIN_BINARIES/log4net.config $PACKAGING_FOLDER
