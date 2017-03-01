e8c9dff62e24a76fbc9e4dc32af60747e29bda21
Chan Jeon <chanjeon@amazon.com>
2017-03-01 14:33:06 -0800

    Added AWS.CloudWatch.log4net.config
    cr https://cr.amazon.com/r/6725505/

--- Tools/src/copy_plugin_binaries.sh
+++ Tools/src/copy_plugin_binaries.sh
@@ -20,3 +20,4 @@ cp $PLUGIN_BINARIES/AWS.CloudWatch.exe.config $BIN_FOLDER
 cp $PLUGIN_BINARIES/AWS.DomainJoin.exe $BIN_FOLDER
 cp $PLUGIN_BINARIES/AWS.DomainJoin.exe.config $BIN_FOLDER
 cp $PLUGIN_BINARIES/log4net.config $BIN_FOLDER
+cp $PLUGIN_BINARIES/AWS.CloudWatch.log4net.config $BIN_FOLDER
