7eed68dfd0132ff21c30fb4bd7d005cd3f4681a6
Dima Germanov <dgermano@amazon.com>
2017-06-05 17:44:13 -0700

    Rename coreplugin files to coremodule

--- agent/message/processor/processor_coremodule.go
+++ agent/message/processor/processor_coremodule.go
@@ -12,7 +12,7 @@
 // permissions and limitations under the License.
 
 // Package processor implements MDS plugin processor
-// processor_coreplugin contains the ICoreModule implementation
+// processor_coremodule contains the ICoreModule implementation
 package processor
 
 import (
