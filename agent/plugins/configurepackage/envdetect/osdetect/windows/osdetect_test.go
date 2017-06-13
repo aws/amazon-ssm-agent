1423a9baed2e7345f16d6e6dc0c49ec0b699d027
Florian Eitel <eitel@amazon.com>
2017-06-13 15:54:11 +0200

    Start with initial windows support in configure package envdetect
    cr https://code.amazon.com/reviews/CR-296487

--- agent/plugins/configurepackage/envdetect/osdetect/windows/osdetect_test.go
+++ agent/plugins/configurepackage/envdetect/osdetect/windows/osdetect_test.go
@@ -22,3 +22,62 @@ func TestDetectInitSystem(t *testing.T) {
 	assert.NoError(t, err)
 	assert.Equal(t, c.InitWindows, result)
 }
+
+func TestParseVersion(t *testing.T) {
+	data := []struct {
+		name            string
+		wmioutput       string
+		expectedVersion string
+		expectError     bool
+	}{
+		{
+			"simple single line version",
+			"Version=10.0.14393",
+			"10.0.14393",
+			false,
+		},
+		{
+			"simple multiline line version",
+			"Version=10.0.14393\n",
+			"10.0.14393",
+			false,
+		},
+		{
+			"whitespace version",
+			"  \t Version  \t  = \t  10.0.14393  \t",
+			"10.0.14393",
+			false,
+		},
+		{
+			"multiple version",
+			"CdVersion=342\nVersion=10.0.14393",
+			"10.0.14393",
+			false,
+		},
+		{
+			"windows newline",
+			"\r\nVersion=10.0.14393\r\n",
+			"10.0.14393",
+			false,
+		},
+		{
+			"empty input",
+			"",
+			"",
+			true,
+		},
+	}
+
+	for _, d := range data {
+		t.Run(d.name, func(t *testing.T) {
+			resultVersion, err := parseVersion(d.wmioutput)
+
+			if d.expectError {
+				assert.True(t, err != nil, "error expected")
+			} else {
+				assert.NoError(t, err)
+				assert.Equal(t, d.expectedVersion, resultVersion)
+			}
+		})
+	}
+}
