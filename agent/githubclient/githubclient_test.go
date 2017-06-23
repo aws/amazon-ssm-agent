a4b4fb7781878f1b47cd60b5e429635c64816b48
Melonia Mendonca <meloniam@amazon.com>
2017-06-23 07:50:13 -0700

    Git access library
    cr https://code.amazon.com/reviews/CR-329462

---
+++ agent/githubclient/githubclient_test.go
@@ -0,0 +1,90 @@
+// Copyright 2017 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"). You may not
+// use this file except in compliance with the License. A copy of the
+// License is located at
+//
+// http://aws.amazon.com/apache2.0/
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+// implied. See the License for the specific language governing
+// permissions and limitations under the License.
+
+// Package githubclient contains methods for interacting with git
+package githubclient
+
+import (
+	"github.com/aws/amazon-ssm-agent/agent/log"
+	"github.com/go-github/github"
+	"github.com/stretchr/testify/assert"
+
+	"testing"
+)
+
+var logMock = log.NewMockLog()
+
+//TODO: Add tests for GetRepositoryContent
+
+func TestGitClient_ParseGetOptions(t *testing.T) {
+	client := NewClient(nil)
+	expected := &github.RepositoryContentGetOptions{
+		Ref: "blah",
+	}
+
+	opt, err := client.ParseGetOptions(logMock, "branch:blah")
+
+	assert.Equal(t, expected, opt)
+	assert.NoError(t, err)
+
+}
+
+func TestGitClient_ParseGetOptionsNoGetOptions(t *testing.T) {
+	client := NewClient(nil)
+	expected := &github.RepositoryContentGetOptions{
+		Ref: "master",
+	}
+
+	opt, err := client.ParseGetOptions(logMock, "")
+
+	assert.Equal(t, expected, opt)
+	assert.NoError(t, err)
+
+}
+
+func TestGitClient_ParseGetOptionsInvalidFormat(t *testing.T) {
+	client := NewClient(nil)
+	var expected *github.RepositoryContentGetOptions
+	expected = nil
+
+	opt, err := client.ParseGetOptions(logMock, "commit:qwertaydhfdig")
+
+	assert.Error(t, err)
+	assert.Equal(t, expected, opt)
+
+}
+
+func TestGitClient_ParseGetOptionsTooManyOptions(t *testing.T) {
+	client := NewClient(nil)
+	var expected *github.RepositoryContentGetOptions
+	expected = nil
+
+	opt, err := client.ParseGetOptions(logMock, "branch:master; commitID:qwertaydhfdig")
+
+	assert.Error(t, err)
+	assert.Equal(t, expected, opt)
+
+}
+
+func TestGitClient_ParseGetOptionsInvalidOptions(t *testing.T) {
+	client := NewClient(nil)
+	var expected *github.RepositoryContentGetOptions
+	expected = nil
+
+	opt, err := client.ParseGetOptions(logMock, "branch")
+
+	assert.Error(t, err)
+	assert.Equal(t, expected, opt)
+
+}
