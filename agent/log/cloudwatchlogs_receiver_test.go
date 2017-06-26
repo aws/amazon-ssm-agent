d53fd566cc1a5ac7c000214910d65b7eb8580560
Kapoor <kaporahu@amazon.com>
2017-06-26 16:00:07 -0700

    Adding Seelog CustomReceiver For Enqueueing Messages to CloudWatchLogs Queue
    cr https://code.amazon.com/reviews/CR-335905

---
+++ agent/log/cloudwatchlogs_receiver_test.go
@@ -0,0 +1,63 @@
+// Copyright 2016 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"). You may not
+// use this file except in compliance with the License. A copy of the
+// License is located at
+//
+// http://aws.amazon.com/apache2.0/
+//
+// or in the "license" file accompanying this file. This file is distributed
+// on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+// either express or implied. See the License for the specific language governing
+// permissions and limitations under the License.
+
+// cloudwatchlogsqueue queues up agent's context event log, to be consumed by the CloudWatchLogs publisher
+
+package log
+
+import (
+	"testing"
+
+	"time"
+
+	"github.com/aws/amazon-ssm-agent/agent/cloudwatchlogsqueue"
+	"github.com/cihub/seelog"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCloudWatchLogsReceiver(t *testing.T) {
+	xmlCustomAttrs := make(map[string]string)
+	xmlCustomAttrs["log-group"] = "LogGroup"
+	xmlCustomAttrs["log-stream"] = "LogStream"
+
+	initArgs := seelog.CustomReceiverInitArgs{
+		XmlCustomAttrs: xmlCustomAttrs,
+	}
+
+	cwLogReceiver := CloudWatchCustomReceiver{}
+
+	cwLogReceiver.AfterParse(initArgs)
+
+	messages, _ := cloudwatchlogsqueue.Dequeue(time.Millisecond)
+
+	assert.Nil(t, messages, "No Messages should be present")
+
+	assert.Equal(t, "LogGroup", cloudwatchlogsqueue.GetLogGroup(), "LogGroup Name Incorrect")
+	assert.Equal(t, "LogStream", cloudwatchlogsqueue.GetLogStream(), "LogStream Name Incorrect")
+
+	cwLogReceiver.ReceiveMessage("Message", seelog.DebugLvl, nil)
+
+	messages, _ = cloudwatchlogsqueue.Dequeue(time.Millisecond)
+
+	assert.NotNil(t, messages, "Messages should be present")
+
+	assert.Len(t, messages, 1, "Messages should be of length 1")
+
+	assert.Equal(t, "Message", *messages[0].Message, "Message Incorrect")
+
+	cwLogReceiver.Close()
+
+	messages, _ = cloudwatchlogsqueue.Dequeue(time.Millisecond)
+	assert.Nil(t, messages, "No Messages should be present")
+
+}
