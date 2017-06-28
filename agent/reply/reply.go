152cb19ff81d3b20e7ea246f8797a3c1d24b0f5c
Wang <shengmiw@amazon.com>
2017-06-27 21:48:28 -0700

    add replybuilder to accumulate plugin results at service end
    cr https://code.amazon.com/reviews/CR-342653

--- agent/reply/reply.go
+++ agent/reply/reply.go
@@ -24,6 +24,38 @@ import (
 	"github.com/aws/amazon-ssm-agent/agent/times"
 )
 
+//TODO once we remove the callback "SendReply", use this class to build reply
+//ReplyBuilder is used by RunCommand Service to accumulate plugin updates, and format SendReply payload
+type ReplyBuilder interface {
+	// UpdatePluginResult updates the internal store with the latest update PluginResult
+	UpdatePluginResult(res contracts.PluginResult) error
+	// Format the Payload send back to MDS service
+	FormatPayload(log.T, string, contracts.AgentInfo) messageContracts.SendReplyPayload
+}
+
+//SendReplyBuilder impl ReplyBuilder used by MDS service to receive updates and send reply payload
+type SendReplyBuilder struct {
+	pluginResults map[string]*contracts.PluginResult
+}
+
+func NewSendReplyBuilder() SendReplyBuilder {
+	return SendReplyBuilder{
+		pluginResults: make(map[string]*contracts.PluginResult),
+	}
+}
+
+// UpdatePluginResult the internal plugin map with the latest Result
+func (builder SendReplyBuilder) UpdatePluginResult(res contracts.PluginResult) error {
+	builder.pluginResults[res.PluginName] = &res
+	return nil
+}
+
+// build SendReply Payload from the internal plugins map
+func (builder SendReplyBuilder) FormatPayload(log log.T, pluginID string, agentInfo contracts.AgentInfo) messageContracts.SendReplyPayload {
+	statuses := PrepareRuntimeStatuses(log, builder.pluginResults)
+	return PrepareReplyPayload(pluginID, statuses, time.Now(), agentInfo)
+}
+
 // PrepareReplyPayload creates the payload object for SendReply based on plugin outputs.
 func PrepareReplyPayload(pluginID string,
 	runtimeStatuses map[string]*contracts.PluginRuntimeStatus,
