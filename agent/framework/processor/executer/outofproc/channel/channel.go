092cf52929cff6c3947fa3ed2ae08a786cd47373
Wang <shengmiw@amazon.com>
2017-07-31 17:00:06 -0700

    initial implementation of file watcher ipc channel
    cr https://code.amazon.com/reviews/CR-456075

--- agent/framework/processor/executer/outofproc/channel/channel.go
+++ agent/framework/processor/executer/outofproc/channel/channel.go
@@ -1,19 +1,22 @@
 package channel
 
-import "github.com/aws/amazon-ssm-agent/agent/framework/processor/executer/outofproc/contracts"
-
 const (
-	ModeClient Mode = "client"
-	ModeServer Mode = "server"
+	ModeMaster Mode = "master"
+	ModeSlave  Mode = "slave"
+)
+const (
+	defaultReceiveChannelBufferSize = 100
 )
 
 type Mode string
 type ChannelCreator func(handle string, mode Mode) (Channel, error)
 
-//TODO this interface is designed to adopt both file and named pipe
+//Channel is an interface for raw json datagram transmission, it is designed to adopt both file ad named pipe
 type Channel interface {
-	Connect() error
-	Send(contracts.Message) error
-	GetMessageChannel() chan contracts.Message
+	//open a named channel, system call is issued at this call
+	Open(string) error
+	//send a raw json datagram to the channel
+	Send(string) error
+	GetMessageChannel() chan string
 	Close()
 }
