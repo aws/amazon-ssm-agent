e5dd698484e12873bc6646e85a168a650137f0f5
Wang <shengmiw@amazon.com>
2017-08-08 12:28:19 -0700

    remove time-dependent unitests
    cr https://code.amazon.com/reviews/CR-487286

--- agent/agentlogstocloudwatch/cloudwatchlogspublisher/cloudwatchlogs_publisher_test.go
+++ agent/agentlogstocloudwatch/cloudwatchlogspublisher/cloudwatchlogs_publisher_test.go
@@ -17,69 +17,16 @@ package cloudwatchlogspublisher
 
 import (
 	"errors"
-	"fmt"
 	"testing"
-	"time"
 
 	"github.com/aws/amazon-ssm-agent/agent/agentlogstocloudwatch/cloudwatchlogspublisher/mock"
 	"github.com/aws/amazon-ssm-agent/agent/agentlogstocloudwatch/cloudwatchlogsqueue"
-	"github.com/aws/aws-sdk-go/service/cloudwatchlogs"
 	"github.com/cihub/seelog"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 )
 
 var serviceMock = cloudwatchlogspublisher_mock.NewServiceMockDefault()
-
-func TestCWPublisherDequeueMessages(t *testing.T) {
-
-	sequenceToken := "1234"
-
-	serviceMock.On("GetSequenceTokenForStream", mock.AnythingOfType("*log.Mock"), mock.AnythingOfType("string"), mock.AnythingOfType("string")).Return(&sequenceToken)
-	serviceMock.On("PutLogEvents",
-		mock.AnythingOfType("*log.Mock"),
-		mock.AnythingOfType("[]*cloudwatchlogs.InputLogEvent"),
-		mock.AnythingOfType("string"),
-		mock.AnythingOfType("string"),
-		mock.AnythingOfType("*string"),
-	).Return(&sequenceToken, nil)
-	serviceMock.On("IsLogGroupPresent", mock.AnythingOfType("*log.Mock"), mock.AnythingOfType("string")).Return(true)
-	serviceMock.On("IsLogStreamPresent", mock.AnythingOfType("*log.Mock"), mock.AnythingOfType("string"), mock.AnythingOfType("string")).Return(true)
-
-	cwPublisher := CloudWatchPublisher{
-		log: logMock,
-		cloudWatchLogsService: serviceMock,
-		QueuePollingInterval:  time.Millisecond * 100,
-		QueuePollingWaitTime:  time.Millisecond,
-		instanceID:            "InstanceID",
-	}
-
-	xmlArgs := make(map[string]string)
-	xmlArgs["log-group"] = "LogGroup"
-
-	initArgs := seelog.CustomReceiverInitArgs{
-		XmlCustomAttrs: xmlArgs,
-	}
-
-	cloudwatchlogsqueue.DestroyCloudWatchDataInstance()
-	cloudwatchlogsqueue.CreateCloudWatchDataInstance(initArgs)
-
-	message := &cloudwatchlogs.InputLogEvent{}
-
-	cloudwatchlogsqueue.Enqueue(message)
-
-	// Start the publisher to test whether it dequeues messages from queue
-	cwPublisher.Start()
-
-	// Starting a 200 millisecond timer to let the publisher dequeue messages
-	time.Sleep(time.Millisecond * 200)
-
-	cwPublisher.Stop()
-
-	// Testing whether messages are still in the queue
-	messages, _ := cloudwatchlogsqueue.Dequeue(time.Millisecond)
-	assert.Nil(t, messages, "Publisher failed to dequeue messages")
-}
 
 func TestCreateLogGroupError(t *testing.T) {
 	serviceMock := cloudwatchlogspublisher_mock.NewServiceMockDefault()
@@ -111,68 +58,6 @@ func TestCreateLogStreamError(t *testing.T) {
 
 	err := cwPublisher.createLogGroupAndStream("Group", "StreamDoesNotExist")
 	assert.Error(t, err, "Error Expected When Log Stream Creation Errors")
-
-}
-
-func TestCloudWatchLogsEventsListener(t *testing.T) {
-	serviceMock := cloudwatchlogspublisher_mock.NewServiceMockDefault()
-	sequenceToken := "1234"
-
-	serviceMock.On("GetSequenceTokenForStream", mock.AnythingOfType("*log.Mock"), mock.AnythingOfType("string"), mock.AnythingOfType("string")).Return(&sequenceToken)
-	serviceMock.On("PutLogEvents",
-		mock.AnythingOfType("*log.Mock"),
-		mock.AnythingOfType("[]*cloudwatchlogs.InputLogEvent"),
-		mock.AnythingOfType("string"),
-		mock.AnythingOfType("string"),
-		mock.AnythingOfType("*string"),
-	).Return(&sequenceToken, nil)
-	serviceMock.On("IsLogGroupPresent", mock.AnythingOfType("*log.Mock"), mock.AnythingOfType("string")).Return(true)
-	serviceMock.On("IsLogStreamPresent", mock.AnythingOfType("*log.Mock"), mock.AnythingOfType("string"), mock.AnythingOfType("string")).Return(true)
-
-	// Create a queue
-	xmlArgs := make(map[string]string)
-	xmlArgs["log-group"] = "LogGroup"
-	initArgs := seelog.CustomReceiverInitArgs{
-		XmlCustomAttrs: xmlArgs,
-	}
-
-	cloudwatchlogsqueue.DestroyCloudWatchDataInstance()
-
-	// Create the publisher
-	cwPublisher := CloudWatchPublisher{
-		log: logMock,
-		cloudWatchLogsService: serviceMock,
-		QueuePollingInterval:  time.Millisecond * 100,
-		QueuePollingWaitTime:  time.Millisecond,
-		instanceID:            "InstanceID",
-	}
-	cwPublisher.Init(logMock)
-
-	message := &cloudwatchlogs.InputLogEvent{}
-	cloudwatchlogsqueue.CreateCloudWatchDataInstance(initArgs)
-	cloudwatchlogsqueue.Enqueue(message)
-
-	// Send Event on channel. This should start the publisher to dequeue messages
-	cloudwatchlogsqueue.CloudWatchLogsEventsChannel <- cloudwatchlogsqueue.QueueActivated
-
-	// Starting a 200 millisecond timer to let the publisher dequeue messages
-	time.Sleep(time.Millisecond * 200)
-
-	// Testing whether messages are still in the queue
-	messages, _ := cloudwatchlogsqueue.Dequeue(time.Millisecond)
-	assert.Nil(t, messages, "Failed to signal publisher to start")
-
-	// Send Event on channel. This should stop the publisher
-	cloudwatchlogsqueue.CloudWatchLogsEventsChannel <- cloudwatchlogsqueue.QueueDeactivated
-	time.Sleep(time.Millisecond * 200)
-
-	cloudwatchlogsqueue.Enqueue(message)
-	// Starting a 200 millisecond timer to let the publisher dequeue messages
-	time.Sleep(time.Millisecond * 200)
-	messages, _ = cloudwatchlogsqueue.Dequeue(time.Millisecond)
-	fmt.Println(messages)
-	assert.NotNil(t, messages, "Failed to signal Publisher to Stop")
-	cwPublisher.Stop()
 
 }
 
@@ -209,72 +94,4 @@ func TestGetSharingConfigurationsIncorrect(t *testing.T) {
 	cloudwatchlogsqueue.CreateCloudWatchDataInstance(initArgs)
 	sharingConfigs := getSharingConfigurations()
 	assert.Nil(t, sharingConfigs, "Configurations should be nil as incorrectly formatted")
-}
-
-func TestStopSharingOnAccessError(t *testing.T) {
-	serviceMock := cloudwatchlogspublisher_mock.NewServiceMockDefault()
-	sequenceToken := "1234"
-	sharingSequenceToken := "12345"
-	serviceMock.On("GetSequenceTokenForStream", mock.AnythingOfType("*log.Mock"), mock.AnythingOfType("string"), mock.AnythingOfType("string")).Return(&sequenceToken)
-	serviceMock.On("PutLogEvents",
-		mock.AnythingOfType("*log.Mock"),
-		mock.AnythingOfType("[]*cloudwatchlogs.InputLogEvent"),
-		mock.AnythingOfType("string"),
-		mock.AnythingOfType("string"),
-		mock.AnythingOfType("*string"),
-	).Return(&sequenceToken, nil)
-
-	sharingServiceMock := cloudwatchlogspublisher_mock.NewServiceMockDefault()
-
-	sharingServiceMock.On("GetSequenceTokenForStream", mock.AnythingOfType("*log.Mock"), mock.AnythingOfType("string"), mock.AnythingOfType("string")).Return(nil)
-	sharingServiceMock.On("PutLogEvents",
-		mock.AnythingOfType("*log.Mock"),
-		mock.AnythingOfType("[]*cloudwatchlogs.InputLogEvent"),
-		mock.AnythingOfType("string"),
-		mock.AnythingOfType("string"),
-		mock.AnythingOfType("*string"),
-	).Return(nil, errors.New("Access Error"))
-
-	cloudwatchlogsqueue.DestroyCloudWatchDataInstance()
-	xmlArgs := make(map[string]string)
-	xmlArgs["log-group"] = "LogGroup"
-
-	initArgs := seelog.CustomReceiverInitArgs{
-		XmlCustomAttrs: xmlArgs,
-	}
-
-	cloudwatchlogsqueue.CreateCloudWatchDataInstance(initArgs)
-
-	// Create the publisher
-	cwPublisher := CloudWatchPublisher{
-		log: logMock,
-		cloudWatchLogsService:        serviceMock,
-		cloudWatchLogsServiceSharing: sharingServiceMock,
-		QueuePollingInterval:         time.Millisecond * 100,
-		QueuePollingWaitTime:         time.Millisecond,
-		isSharingEnabled:             true,
-		selfDestination: &destinationConfigurations{
-			logGroup:  "Group",
-			logStream: "Stream",
-		},
-		sharingDestination: &destinationConfigurations{
-			logGroup:  "GroupSharing",
-			logStream: "StreamSharing",
-		},
-		instanceID: "instanceID",
-	}
-	cwPublisher.startPolling(&sequenceToken, &sharingSequenceToken)
-
-	message := &cloudwatchlogs.InputLogEvent{}
-
-	cloudwatchlogsqueue.Enqueue(message)
-
-	// Starting a 200 millisecond timer to let the publisher dequeue messages
-	time.Sleep(time.Millisecond * 200)
-	cwPublisher.Stop()
-
-	fmt.Println(cwPublisher)
-
-	assert.False(t, cwPublisher.isSharingEnabled, "Sharing should be disabled on Access Error")
-
 }
