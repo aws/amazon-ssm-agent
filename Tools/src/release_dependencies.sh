ac68b78ae88faba672e45a54bcbb442526f6c46e
Melonia Mendonca <meloniam@amazon.com>
2017-08-09 17:27:46 -0700

    Adding cloudwatch DLLs to the packaging dependencies
    cr https://code.amazon.com/reviews/CR-495167

--- Tools/src/release_dependencies.sh
+++ Tools/src/release_dependencies.sh
@@ -6,9 +6,7 @@ echo "********************************************"
 PACKAGING_FOLDER="$BGO_SPACE/packaging/dependencies/"
 PLUGIN_BINARIES="`brazil-path run.configfarm.artifacts`/artifacts"
 
-cp $PLUGIN_BINARIES/AWS.CloudWatch.exe $PACKAGING_FOLDER
-cp $PLUGIN_BINARIES/AWS.CloudWatch.exe.config $PACKAGING_FOLDER
 cp $PLUGIN_BINARIES/AWS.DomainJoin.exe $PACKAGING_FOLDER
 cp $PLUGIN_BINARIES/AWS.DomainJoin.exe.config $PACKAGING_FOLDER
 cp $PLUGIN_BINARIES/log4net.config $PACKAGING_FOLDER
-cp $PLUGIN_BINARIES/AWS.CloudWatch.log4net.config $PACKAGING_FOLDER
+cp -R $PLUGIN_BINARIES/awsCloudwatch/ $PACKAGING_FOLDER
