e539da1257b733042385e1470c617def0cf64997
Wang <shengmiw@amazon.com>
2017-08-18 10:55:00 -0700

    remove Open() from Channel interface
    cr https://code.amazon.com/reviews/CR-533438

--- agent/framework/processor/executer/outofproc/channel/channel.go
+++ agent/framework/processor/executer/outofproc/channel/channel.go
@@ -1,22 +1,56 @@
 package channel
 
+import (
+	"path"
+
+	"github.com/aws/amazon-ssm-agent/agent/appconfig"
+	"github.com/aws/amazon-ssm-agent/agent/fileutil"
+	"github.com/aws/amazon-ssm-agent/agent/log"
+	"github.com/aws/amazon-ssm-agent/build/private/src/github.com/aws/amazon-ssm-agent/agent/platform"
+)
+
 const (
 	ModeMaster Mode = "master"
 	ModeWorker Mode = "worker"
 )
 const (
 	defaultChannelBufferSize = 100
+	defaultFileChannelPath   = "channels"
 )
 
 type Mode string
-type ChannelCreator func(mode Mode) Channel
 
 //Channel is an interface for raw json datagram transmission, it is designed to adopt both file ad named pipe
 type Channel interface {
-	//open a named channel, system call is issued at this call
-	Open(string) error
 	//send a raw json datagram to the channel, should be non-blocked
 	Send(string) error
 	GetMessageChannel() chan string
 	Close()
+}
+
+//find the folder named as "documentID" under the default root dir
+//if not found, create a new filechannel under the default root dir
+//return the channel and the found flag
+func CreateFileChannel(log log.T, mode Mode, filename string) (Channel, error, bool) {
+	instanceID, err := platform.InstanceID()
+	if err != nil {
+		log.Errorf("failed to load instance ID: %v", err)
+		return nil, err, false
+	}
+	list, err := fileutil.ReadDir(path.Join(appconfig.DefaultDataStorePath, instanceID, defaultFileChannelPath))
+	if err != nil {
+		log.Errorf("failed to read the default channel root directory: %v, creating a new Channel", err)
+		f, err := NewFileWatcherChannel(log, mode, path.Join(appconfig.DefaultDataStorePath, instanceID, defaultFileChannelPath, filename))
+		return f, err, false
+	}
+	for _, val := range list {
+		if val.Name() == filename {
+			log.Errorf("channel: %v found", filename)
+			f, err := NewFileWatcherChannel(log, ModeMaster, path.Join(appconfig.DefaultDataStorePath, instanceID, defaultFileChannelPath, filename))
+			return f, err, true
+		}
+	}
+
+	f, err := NewFileWatcherChannel(log, mode, path.Join(appconfig.DefaultDataStorePath, instanceID, defaultFileChannelPath, filename))
+	return f, err, false
 }
