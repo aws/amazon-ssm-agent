632411b2d4d41498e0c1821a210dbec41c8dc553
Melonia Mendonca <meloniam@amazon.com>
2017-09-08 11:28:28 -0700

    Refactoring remote resource factory out to copyContent plugin. Also refactoring filemanager out to a new package
    cr https://code.amazon.com/reviews/CR-616332

--- agent/plugins/copycontent/s3resource/s3resource_test.go
+++ agent/plugins/copycontent/s3resource/s3resource_test.go
@@ -15,19 +15,17 @@
 package s3resource
 
 import (
-	filemock "github.com/aws/amazon-ssm-agent/agent/plugins/executecommand/filemanager/mock"
+	filemock "github.com/aws/amazon-ssm-agent/agent/filemanager/mock"
 
 	"github.com/aws/amazon-ssm-agent/agent/appconfig"
 	"github.com/aws/amazon-ssm-agent/agent/fileutil"
 	"github.com/aws/amazon-ssm-agent/agent/fileutil/artifact"
 	"github.com/aws/amazon-ssm-agent/agent/log"
-	"github.com/aws/amazon-ssm-agent/agent/plugins/executecommand/remoteresource"
 	"github.com/aws/amazon-ssm-agent/agent/s3util"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 
 	"errors"
-	"path/filepath"
 	"testing"
 )
 
@@ -70,16 +68,6 @@ func TestIsFolder_Folder(t *testing.T) {
 	assert.True(t, res)
 }
 
-func TestS3Resource_GetDirectoryURLString(t *testing.T) {
-	resource := &S3Resource{
-		Info: S3Info{Path: "https://s3.amazonaws.com/my-bucket/mydummyfolder/myfile.ps"},
-	}
-
-	res := resource.getDirectoryURLString()
-
-	assert.Equal(t, "https://s3.amazonaws.com/my-bucket/mydummyfolder/", res)
-}
-
 func TestS3Resource_GetS3BucketURLString(t *testing.T) {
 	resource := &S3Resource{
 		Info: S3Info{Path: "https://s3.amazonaws.com/my-bucket/mydummyfolder/myfile.ps"},
@@ -106,104 +94,6 @@ func TestS3Resource_GetS3BucketURLString_sameBucketNameFile(t *testing.T) {
 	assert.Equal(t, "https://s3.amazonaws.com/my-bucket/", res)
 }
 
-func TestS3Resource_GetSourceURL_IsFolder(t *testing.T) {
-	resource := &S3Resource{
-		Info: S3Info{Path: "https://s3.amazonaws.com/my-bucket/mydummyfolder/myfile.ps"},
-	}
-
-	url, err := resource.getSourceURL(logMock, true)
-
-	assert.NoError(t, err)
-	assert.Equal(t, "https://s3.amazonaws.com/my-bucket/mydummyfolder/", url.String())
-}
-
-func TestS3Resource_GetSourceURL_NotFolder(t *testing.T) {
-	resource := &S3Resource{
-		Info: S3Info{Path: "https://s3.amazonaws.com/my-bucket/mydummyfolder/myfile.ps"},
-	}
-
-	url, err := resource.getSourceURL(logMock, false)
-
-	assert.NoError(t, err)
-	assert.Equal(t, "https://s3.amazonaws.com/my-bucket/mydummyfolder/myfile.ps", url.String())
-}
-
-func TestS3Resource_PopulateResourceInfoEntireDirFalseJSON(t *testing.T) {
-	locationInfo := `{
-		"path" : "https://s3.amazonaws.com/my-bucket/mydummyfolder/file.json"
-	}`
-
-	resourceInfo := remoteresource.ResourceInfo{}
-	s3resource, _ := NewS3Resource(logMock, locationInfo)
-	s3resource.s3Object.Bucket = "my-bucket"
-	s3resource.s3Object.Key = "mydummyfolder/file.json"
-
-	resourceInfo = s3resource.PopulateResourceInfo(logMock, "", false)
-
-	assert.Equal(t, remoteresource.Document, resourceInfo.TypeOfResource)
-	assert.False(t, resourceInfo.EntireDir)
-	assert.Equal(t, "file.json", resourceInfo.StarterFile)
-	assert.Equal(t, filepath.Join(appconfig.DownloadRoot, "my-bucket/mydummyfolder/file.json"), resourceInfo.LocalDestinationPath)
-}
-
-func TestS3Resource_PopulateResourceInfoEntireDirTrue(t *testing.T) {
-	locationInfo := `{
-		"path" : "https://s3.amazonaws.com/my-bucket/mydummyfolder/file.rb"
-	}`
-
-	resourceInfo := remoteresource.ResourceInfo{}
-	s3resource, _ := NewS3Resource(logMock, locationInfo)
-
-	s3resource.s3Object.Bucket = "my-bucket"
-	s3resource.s3Object.Key = "mydummyfolder/"
-	resourceInfo = s3resource.PopulateResourceInfo(logMock, "", true)
-
-	assert.True(t, resourceInfo.EntireDir)
-	assert.Equal(t, remoteresource.Script, resourceInfo.TypeOfResource)
-	assert.NotEqual(t, "mydummyfolder/file.rb", resourceInfo.StarterFile)
-	assert.Equal(t, "file.rb", resourceInfo.StarterFile)
-	assert.Equal(t, resourceInfo.LocalDestinationPath, filepath.Join(appconfig.DownloadRoot, "my-bucket/mydummyfolder/file.rb"))
-}
-
-func TestS3Resource_PopulateResourceInfoEntireDirTrueInvalidStarter(t *testing.T) {
-	locationInfo := `{
-		"path" : "https://s3.amazonaws.com/my-bucket/path/to/"
-	}`
-
-	resourceInfo := remoteresource.ResourceInfo{}
-	s3resource, _ := NewS3Resource(logMock, locationInfo)
-
-	s3resource.s3Object.Bucket = "my-bucket"
-	s3resource.s3Object.Key = "path/"
-	resourceInfo = s3resource.PopulateResourceInfo(logMock, "", true)
-
-	assert.True(t, resourceInfo.EntireDir)
-	assert.Equal(t, remoteresource.Script, resourceInfo.TypeOfResource)
-	assert.Equal(t, "to", resourceInfo.StarterFile)
-	assert.Equal(t, resourceInfo.LocalDestinationPath, filepath.Join(appconfig.DownloadRoot, "my-bucket/path/to/"))
-
-}
-
-func TestS3Resource_PopulateResourceInfoEntireDirFalseScript(t *testing.T) {
-
-	locationInfo := `{
-		"path" : "https://s3.amazonaws.com/my-bucket/mydummyfolder/file.rb"
-	}`
-
-	resourceInfo := remoteresource.ResourceInfo{}
-	s3resource, _ := NewS3Resource(logMock, locationInfo)
-
-	s3resource.s3Object.Bucket = "my-bucket"
-	s3resource.s3Object.Key = "mydummyfolder/file.rb"
-	resourceInfo = s3resource.PopulateResourceInfo(logMock, "destination", false)
-
-	assert.False(t, resourceInfo.EntireDir)
-	assert.Equal(t, remoteresource.Script, resourceInfo.TypeOfResource)
-	assert.Equal(t, "file.rb", resourceInfo.StarterFile)
-	assert.Equal(t, resourceInfo.LocalDestinationPath, filepath.Join("destination", "my-bucket", "mydummyfolder/file.rb"))
-
-}
-
 func TestS3Resource_Download(t *testing.T) {
 
 	depMock := new(s3DepMock)
@@ -225,24 +115,24 @@ func TestS3Resource_Download(t *testing.T) {
 	fileMock.On("MoveAndRenameFile", mock.Anything, mock.Anything, mock.Anything, mock.Anything).Return(true, nil)
 
 	dep = depMock
-	err := resource.Download(logMock, fileMock, false, "destination")
+	err := resource.Download(logMock, fileMock, "destination")
 
 	assert.NoError(t, err)
 	depMock.AssertExpectations(t)
 	fileMock.AssertExpectations(t)
 }
 
-func TestS3Resource_DownloadEntireDirFalse(t *testing.T) {
+func TestS3Resource_DownloadWrongPath(t *testing.T) {
 	depMock := new(s3DepMock)
 	locationInfo := `{
-		"Path" : "https://s3.amazonaws.com/my-bucket/mydummyfolder"
+		"Path" : "https://s3.amazonaws.com/my-bucket/filename.ps"
 	}`
 	fileMock := filemock.FileSystemMock{}
 	resource, _ := NewS3Resource(logMock, locationInfo)
 
 	input := artifact.DownloadInput{
-		DestinationDirectory: fileutil.BuildPath("destination", "my-bucket"),
-		SourceURL:            "https://s3.amazonaws.com/my-bucket/mydummyfolder",
+		DestinationDirectory: fileutil.BuildPath(appconfig.DownloadRoot, "my-bucket"),
+		SourceURL:            "https://s3.amazonaws.com/my-bucket/filename.ps",
 	}
 	output := artifact.DownloadOutput{
 		LocalFilePath: input.DestinationDirectory,
@@ -252,7 +142,7 @@ func TestS3Resource_DownloadEntireDirFalse(t *testing.T) {
 	fileMock.On("MoveAndRenameFile", mock.Anything, mock.Anything, mock.Anything, mock.Anything).Return(true, errors.New("unexpected error encountered while moving the file. Error details -"))
 
 	dep = depMock
-	err := resource.Download(logMock, fileMock, false, "destination")
+	err := resource.Download(logMock, fileMock, "")
 
 	assert.Error(t, err)
 	depMock.AssertExpectations(t)
@@ -261,10 +151,10 @@ func TestS3Resource_DownloadEntireDirFalse(t *testing.T) {
 		"possible that the content was not downloaded because the path provided is wrong.")
 }
 
-func TestS3Resource_DownloadEntireDirTrue(t *testing.T) {
+func TestS3Resource_DownloadDirectory(t *testing.T) {
 	depMock := new(s3DepMock)
 	locationInfo := `{
-		"path" : "https://s3.amazonaws.com/my-bucket/mydummyfolder/filename.ps"
+		"path" : "https://s3.amazonaws.com/my-bucket/mydummyfolder/"
 	}`
 	fileMock := filemock.FileSystemMock{}
 	resource, _ := NewS3Resource(logMock, locationInfo)
@@ -297,24 +187,9 @@ func TestS3Resource_DownloadEntireDirTrue(t *testing.T) {
 	fileMock.On("MoveAndRenameFile", mock.Anything, mock.Anything, mock.Anything, mock.Anything).Return(true, nil).Twice()
 
 	dep = depMock
-	err := resource.Download(logMock, fileMock, true, "destination")
+	err := resource.Download(logMock, fileMock, "destination")
 
 	assert.NoError(t, err)
 	depMock.AssertExpectations(t)
 	fileMock.AssertExpectations(t)
-}
-
-func TestS3Resource_DownloadEntireDirTruePathInvalid(t *testing.T) {
-	depMock := new(s3DepMock)
-	locationInfo := `{
-		"path" : "https://s3.amazonaws.com/my-bucket/mydummyfolder/"
-	}`
-	fileMock := filemock.FileSystemMock{}
-	resource, _ := NewS3Resource(logMock, locationInfo)
-
-	dep = depMock
-	err := resource.Download(logMock, fileMock, true, "")
-
-	assert.Error(t, err)
-	assert.Equal(t, "Could not download from S3. Please provide path to file with extention.", err.Error())
 }
