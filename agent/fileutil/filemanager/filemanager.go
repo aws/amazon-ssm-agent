cede20ab4c2cb4fe86f6e5c200af70d4423370eb
Melonia Mendonca <meloniam@amazon.com>
2017-09-12 15:39:26 -0700

    Refactoring filemanager into a new package since it didn't happen in earlier commit
    cr https://code.amazon.com/reviews/CR-631278

---
+++ agent/fileutil/filemanager/filemanager.go
@@ -0,0 +1,69 @@
+// Copyright 2017 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"). You may not
+// use this file except in compliance with the License. A copy of the
+// License is located at
+//
+// http://aws.amazon.com/apache2.0/
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+// implied. See the License for the specific language governing
+// permissions and limitations under the License.
+
+//TODO: meloniam@ https://sim.amazon.com/issues/SSM-3322
+//TODO: This package is a start to migration of the fileutil code to be inside an interface for better mocking.
+// Package filemanager have all the file related dependencies used by the execute package
+package filemanager
+
+import (
+	"github.com/aws/amazon-ssm-agent/agent/fileutil"
+)
+
+// FileSystem implements dependency on filesystem and os utility functions
+type FileSystem interface {
+	MakeDirs(destinationDir string) (err error)
+	WriteFile(filename string, content string) error
+	ReadFile(filename string) (string, error)
+	MoveAndRenameFile(sourcePath, sourceName, destPath, destName string) (result bool, err error)
+	DeleteFile(filename string) (err error)
+	DeleteDirectory(filename string) (err error)
+	Exists(filename string) bool
+}
+
+type FileSystemImpl struct{}
+
+// MakeDirs creates a directory with execute access
+func (f FileSystemImpl) MakeDirs(destinationDir string) (err error) {
+	return fileutil.MakeDirsWithExecuteAccess(destinationDir)
+}
+
+// MoveAndRenameFile moves and renames the file
+func (f FileSystemImpl) MoveAndRenameFile(sourcePath, sourceName, destPath, destName string) (result bool, err error) {
+	return fileutil.MoveAndRenameFile(sourcePath, sourceName, destPath, destName)
+}
+
+// DeleteFile deletes the file
+func (f FileSystemImpl) DeleteFile(filename string) (err error) {
+	return fileutil.DeleteFile(filename)
+}
+
+// DeleteDirectory deletes the file
+func (f FileSystemImpl) DeleteDirectory(filename string) (err error) {
+	return fileutil.DeleteDirectory(filename)
+}
+
+// WriteFile writes the content in the file path provided
+func (f FileSystemImpl) WriteFile(filename string, content string) error {
+	return fileutil.WriteAllText(filename, content)
+}
+
+// ReadFile reads the contents of file in path provided
+func (f FileSystemImpl) ReadFile(filename string) (string, error) {
+	return fileutil.ReadAllText(filename)
+}
+
+func (f FileSystemImpl) Exists(root string) bool {
+	return fileutil.Exists(root)
+}
