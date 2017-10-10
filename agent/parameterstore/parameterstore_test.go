1a727a7485daf987280539e37beba32023ddf5dd
Mohite <sumohite@amazon.com>
2017-10-10 00:35:39 -0700

    Allow referencing SSM Parameters with versions
    cr https://cr.amazon.com/r/7812062/

--- agent/parameterstore/parameterstore_test.go
+++ agent/parameterstore/parameterstore_test.go
@@ -41,9 +41,10 @@ var StringTestCases = []StringTestCase{
 		Output: "This is a testvalue string",
 		Parameters: []Parameter{
 			{
-				Name:  "test",
-				Type:  "String",
-				Value: "testvalue",
+				Name:    "test",
+				Type:    "String",
+				Value:   "testvalue",
+				Version: 5,
 			},
 		},
 		InvalidParameters: []string{},
@@ -53,9 +54,10 @@ var StringTestCases = []StringTestCase{
 		Output: "This is a testvalueWithDot with dot string",
 		Parameters: []Parameter{
 			{
-				Name:  "test.p1",
-				Type:  "String",
-				Value: "testvalueWithDot",
+				Name:    "test.p1",
+				Type:    "String",
+				Value:   "testvalueWithDot",
+				Version: 5,
 			},
 		},
 		InvalidParameters: []string{},
@@ -65,9 +67,10 @@ var StringTestCases = []StringTestCase{
 		Output: "This is a testValueWithDash with dash string",
 		Parameters: []Parameter{
 			{
-				Name:  "test-p1",
-				Type:  "String",
-				Value: "testValueWithDash",
+				Name:    "test-p1",
+				Type:    "String",
+				Value:   "testValueWithDash",
+				Version: 5,
 			},
 		},
 		InvalidParameters: []string{},
@@ -77,23 +80,46 @@ var StringTestCases = []StringTestCase{
 		Output: "This is a testValueWithSlash with slash string",
 		Parameters: []Parameter{
 			{
-				Name:  "test/p1",
-				Type:  "String",
-				Value: "testValueWithSlash",
+				Name:    "test/p1",
+				Type:    "String",
+				Value:   "testValueWithSlash",
+				Version: 5,
 			},
 		},
 		InvalidParameters: []string{},
 	}, StringTestCase{
-		Input:  "This is a {{ssm:test/p5}} that does not exist",
-		Output: "This is a {{ssm:test/p5}} that does not exist",
+		Input:  "This is a {{ssm:test:4}} with version",
+		Output: "This is a value with version",
 		Parameters: []Parameter{
 			{
-				Name:  "testp1",
-				Type:  "String",
-				Value: "testValueWithSlash",
+				Name:    "test",
+				Type:    "String",
+				Value:   "value",
+				Version: 4,
 			},
 		},
 		InvalidParameters: []string{},
+	}, StringTestCase{
+		Input:  "This is a {{ssm:/test/foo:5}} with version",
+		Output: "This is a value with version",
+		Parameters: []Parameter{
+			{
+				Name:    "/test/foo",
+				Type:    "String",
+				Value:   "value",
+				Version: 5,
+			},
+		},
+		InvalidParameters: []string{},
+	},
+}
+
+var InvalidStringTestCases = []StringTestCase{
+	StringTestCase{
+		Input:             "This is a {{ssm:test/p5}} that does not exist",
+		Output:            "This is a {{ssm:test/p5}} that does not exist",
+		Parameters:        []Parameter{},
+		InvalidParameters: []string{"test/p5"},
 	},
 }
 
@@ -102,6 +128,10 @@ var logger = log.NewMockLog()
 func TestResolve(t *testing.T) {
 	for _, testCase := range StringTestCases {
 		testResolveMethod(t, testCase)
+	}
+
+	for _, testCase := range InvalidStringTestCases {
+		testResolveMethodWithInvalidCase(t, testCase)
 	}
 }
 
@@ -121,6 +151,21 @@ func testResolveMethod(t *testing.T, testCase StringTestCase) {
 	assert.Nil(t, err)
 }
 
+func testResolveMethodWithInvalidCase(t *testing.T, testCase StringTestCase) {
+	callParameterService = func(
+		log log.T,
+		paramNames []string) (*GetParametersResponse, error) {
+		result := GetParametersResponse{}
+		result.Parameters = testCase.Parameters
+		result.InvalidParameters = testCase.InvalidParameters
+		return &result, nil
+	}
+
+	_, err := Resolve(logger, testCase.Input)
+
+	assert.NotNil(t, err)
+}
+
 func testGetValidSSMParamRegexCompiler(t *testing.T) {
 	validSSMParam, _ := getValidSSMParamRegexCompiler(logger, "test.p1")
 	assert.True(t, validSSMParam.MatchString("test.p1"), "test.p1 should not match test.p1")
@@ -128,8 +173,12 @@ func testGetValidSSMParamRegexCompiler(t *testing.T) {
 	assert.False(t, validSSMParam.MatchString("testap1"), "test.p1 should not match test-p1")
 	validSSMParam2, _ := getValidSSMParamRegexCompiler(logger, "test.p1.p2.p3")
 	assert.True(t, validSSMParam2.MatchString("test.p1.p2.p3"), "test.p1.p2.p3 should not match test.p1.p2.p3")
-	assert.False(t, validSSMParam2.MatchString("test.p1-p2-p3"), "test.p1.p2.p3  should not match test.p1-p2-p3")
+	assert.False(t, validSSMParam2.MatchString("test.p1-p2-p3"), "test.p1.p2.p3 should not match test.p1-p2-p3")
 	assert.False(t, validSSMParam2.MatchString("test.p1.p2-p3"), "test.p1.p2.p3 should not match test.p1.p2-p3")
+	validSSMParam3, _ := getValidSSMParamRegexCompiler(logger, "test:3")
+	assert.True(t, validSSMParam3.MatchString("test:3"), "test:3 should not match test:3")
+	assert.False(t, validSSMParam3.MatchString("test.p1-p2-p3"), "test:3 should not match test.p1-p2-p3")
+	assert.False(t, validSSMParam3.MatchString("test.p1.p2-p3"), "test:3 should not match test.p1.p2-p3")
 }
 
 func TestValidateSSMParameters(t *testing.T) {
@@ -239,5 +288,127 @@ func TestValidateSSMParameters(t *testing.T) {
 
 	err = ValidateSSMParameters(logger, documentParameters, parameters)
 	assert.Equal(t, "Parameter value for commands does not match the allowed pattern ^[a-zA-Z]+$", err.Error())
+
+	// Test case 4 with SSM parameter versions
+	documentParameters = map[string]*contracts.Parameter{
+		"commands": {
+			AllowedPattern: "^[a-zA-Z]+$",
+			ParamType:      ParamTypeString,
+		},
+		"workingDirectory": {
+			AllowedPattern: "",
+		},
+		"testDirectory": {
+			AllowedPattern: "",
+		},
+		"someDirectory": {
+			AllowedPattern: "",
+		},
+	}
+
+	parameters = map[string]interface{}{
+		"commands":         "{{ssm:test:3}}",
+		"workingDirectory": "{{ssm:test:5}}",
+		"testDirectory":    "{{ssm:test}}",
+		"someDirectory":    "{{ssm:foo:7}}",
+	}
+
+	ssmParameters = []Parameter{
+		{
+			Name:    "test",
+			Type:    ParamTypeString,
+			Value:   "ls",
+			Version: 3,
+		},
+		{
+			Name:    "test",
+			Type:    ParamTypeString,
+			Value:   "date",
+			Version: 5,
+		},
+		{
+			Name:    "test",
+			Type:    ParamTypeString,
+			Value:   "date",
+			Version: 5,
+		},
+		{
+			Name:    "foo",
+			Type:    ParamTypeString,
+			Value:   "1234",
+			Version: 7,
+		},
+	}
+
+	callParameterService = func(
+		log log.T,
+		paramNames []string) (*GetParametersResponse, error) {
+		result := GetParametersResponse{}
+		result.Parameters = ssmParameters
+		result.InvalidParameters = invalidSSMParameters
+		return &result, nil
+	}
+
+	err = ValidateSSMParameters(logger, documentParameters, parameters)
+	assert.Nil(t, err)
+
+	// Test case 4 with invalid SSM parameter versions
+	documentParameters = map[string]*contracts.Parameter{
+		"commands": {
+			AllowedPattern: "^[a-zA-Z]+$",
+			ParamType:      ParamTypeString,
+		},
+		"workingDirectory": {
+			AllowedPattern: "",
+		},
+		"testDirectory": {
+			AllowedPattern: "",
+		},
+		"someDirectory": {
+			AllowedPattern: "",
+		},
+	}
+
+	parameters = map[string]interface{}{
+		"commands":         "{{ssm:test:3}}",
+		"workingDirectory": "{{ssm:test:5}}",
+		"testDirectory":    "{{ssm:test}}",
+		"someDirectory":    "{{ssm:foo:7}}",
+	}
+
+	ssmParameters = []Parameter{
+		{
+			Name:    "test",
+			Type:    ParamTypeString,
+			Value:   "ls",
+			Version: 3,
+		},
+		{
+			Name:    "test",
+			Type:    ParamTypeString,
+			Value:   "date",
+			Version: 5,
+		},
+		{
+			Name:    "test",
+			Type:    ParamTypeString,
+			Value:   "date",
+			Version: 5,
+		},
+	}
+
+	invalidSSMParameters = []string{"foo:7"}
+
+	callParameterService = func(
+		log log.T,
+		paramNames []string) (*GetParametersResponse, error) {
+		result := GetParametersResponse{}
+		result.Parameters = ssmParameters
+		result.InvalidParameters = invalidSSMParameters
+		return &result, nil
+	}
+
+	err = ValidateSSMParameters(logger, documentParameters, parameters)
+	assert.Equal(t, "Input contains invalid parameters [foo:7]", err.Error())
 
 }
