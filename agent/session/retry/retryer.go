f2f75613fd6a02933887488e059de992c80eb3d5
Zhang <yujiaoz@amazon.com>
2018-07-24 11:57:08 -0700

    Implemented retryer which provides backoff retry strategy.
    cr https://code.amazon.com/reviews/CR-2681317

---
+++ agent/session/retry/retryer.go
@@ -0,0 +1,58 @@
+// Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"). You may not
+// use this file except in compliance with the License. A copy of the
+// License is located at
+//
+// http://aws.amazon.com/apache2.0/
+//
+// or in the "license" file accompanying this file. This file is distributed
+// on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+// either express or implied. See the License for the specific language governing
+// permissions and limitations under the License.
+
+// Package retry implements back off retry strategy for session manager channel connection.
+package retry
+
+import (
+	"math"
+	"time"
+)
+
+type Retryer interface {
+	Call() error
+	NextSleepTime(int32) time.Duration
+}
+
+type RepeatableExponentialRetryer struct {
+	CallableFunc        func() (interface{}, error)
+	GeometricRatio      float64
+	InitialDelayInMilli int
+	MaxDelayInMilli     int
+	MaxAttempts         int
+}
+
+// NextSleepTime calculates the next delay of retry.
+func (retryer *RepeatableExponentialRetryer) NextSleepTime(attempt int) time.Duration {
+	return time.Duration(float64(retryer.InitialDelayInMilli)*math.Pow(retryer.GeometricRatio, float64(attempt))) * time.Millisecond
+}
+
+// Call calls the operation and does exponential retry if error happens until it reaches MaxAttempts if specified.
+func (retryer *RepeatableExponentialRetryer) Call() (channel interface{}, err error) {
+	attempt := 0
+	failedAttemptsSoFar := 0
+	for {
+		channel, err := retryer.CallableFunc()
+		if err == nil || failedAttemptsSoFar == retryer.MaxAttempts {
+			return channel, err
+		}
+		sleep := retryer.NextSleepTime(attempt)
+		if int(sleep/time.Millisecond) > retryer.MaxDelayInMilli {
+			attempt = 0
+			sleep = retryer.NextSleepTime(attempt)
+		}
+		time.Sleep(sleep)
+		attempt++
+		failedAttemptsSoFar++
+	}
+}
---
+++ agent/session/retry/retryer_test.go
@@ -0,0 +1,54 @@
+// Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"). You may not
+// use this file except in compliance with the License. A copy of the
+// License is located at
+//
+// http://aws.amazon.com/apache2.0/
+//
+// or in the "license" file accompanying this file. This file is distributed
+// on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+// either express or implied. See the License for the specific language governing
+// permissions and limitations under the License.
+
+// Package retry implements back off retry strategy for session manager channel connection.
+package retry
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type RetryCounter struct {
+	TotalAttempts int
+}
+
+var (
+	retryGeometricRatio = 2.0
+	initialDelayInMilli = 100
+	maxDelayInMilli     = 1000
+	maxAttempts         = 5
+	totalAttempts       = 0
+	callableFunc        = func() (interface{}, error) {
+		totalAttempts = totalAttempts + 1
+		return RetryCounter{totalAttempts}, errors.New("error occured in callable function")
+	}
+)
+
+func TestRepeatableExponentialRetryerRetriesForGivenNumberOfMaxAttempts(t *testing.T) {
+	retryer := RepeatableExponentialRetryer{
+		callableFunc,
+		retryGeometricRatio,
+		initialDelayInMilli,
+		maxDelayInMilli,
+		maxAttempts,
+	}
+
+	retryCounterInterface, err := retryer.Call()
+
+	retryCounter := retryCounterInterface.(RetryCounter)
+	assert.NotNil(t, err)
+	assert.Equal(t, retryCounter.TotalAttempts, maxAttempts+1)
+}
