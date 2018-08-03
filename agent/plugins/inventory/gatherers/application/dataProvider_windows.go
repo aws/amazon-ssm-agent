5926231db414a77b9d902cfdba07a70c99052af6
Li <yuanqian@amazon.com>
2018-08-03 11:19:22 -0700

    Bug fix: Fix converting issue when there are control codes exist in the result of gathering application name prcoess. Add one method to remove UTF-8 control code.

--- agent/plugins/inventory/gatherers/application/dataProvider_windows.go
+++ agent/plugins/inventory/gatherers/application/dataProvider_windows.go
@@ -271,7 +271,8 @@ func executePowershellCommands(context context.T, command, args, arch string) (d
 		log.Error(err.Error())
 		log.Infof("No application data to return")
 	} else {
-		cmdOutput := string(output)
+		// Clean up all invisible codes in byte array
+		cmdOutput := stripCtlFromUTF8(string(output))
 		log.Debugf("Command output: %v", cmdOutput)
 
 		if data, err = convertToApplicationData(cmdOutput, arch); err != nil {
