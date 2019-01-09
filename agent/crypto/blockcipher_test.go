30578b142fc9b2fffb89cb111d993ee26303a736
Akshay Singhvi <singhvia@amazon.com>
2019-01-09 13:25:19 -0800

    Add encryption primitives for SessionManager
    cr https://code.amazon.com/reviews/CR-4432955

---
+++ agent/crypto/blockcipher_test.go
@@ -0,0 +1,82 @@
+// Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"). You may not
+// use this file except in compliance with the License. A copy of the
+// License is located at
+//
+// http://aws.amazon.com/apache2.0/
+//
+// or in the "license" file accompanying this file. This file is distributed
+// on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+// either express or implied. See the License for the specific language governing
+// permissions and limitations under the License.
+
+// crypto package provides methods to encrypt and decrypt data
+package crypto
+
+import (
+	"encoding/hex"
+	"testing"
+
+	"github.com/aws/amazon-ssm-agent/agent/crypto/mocks"
+	"github.com/aws/amazon-ssm-agent/agent/log"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
+)
+
+type BlockCipherTestSuite struct {
+	suite.Suite
+	mockLog        log.T
+	mockKMSService mocks.IKMSService
+	kmsKeyId       string
+	plainTextData  []byte
+	cipherTextKey  []byte
+	plainTextKey   []byte
+}
+
+func (suite *BlockCipherTestSuite) SetupTest() {
+	suite.mockLog = log.NewMockLog()
+	suite.mockKMSService = mocks.IKMSService{}
+
+	suite.kmsKeyId = "kmsKeyId"
+	suite.plainTextData = []byte("plainTextDataToBeEncrypted")
+	suite.cipherTextKey = []byte("cipherTextKey")
+	suite.plainTextKey, _ = hex.DecodeString("7775626261206c756262612064756220647562207775626261206c756262612064756220647562207775626261206c7562626120647562206475622077756262")
+
+}
+
+//Execute the test suite
+func TestBlockCipherTestSuite(t *testing.T) {
+	suite.Run(t, new(BlockCipherTestSuite))
+}
+
+// Testing Encrypt and Decrypt functions
+func (suite *BlockCipherTestSuite) TestEncryptDecrypt() {
+
+	suite.mockKMSService.On("GenerateDataKey", suite.kmsKeyId).Return(suite.cipherTextKey, suite.plainTextKey, nil)
+
+	blockCipher, err := NewBlockCipherKMS(suite.mockLog, suite.kmsKeyId, &suite.mockKMSService)
+	assert.Nil(suite.T(), err)
+	// Create another cipher with flipped encryption/decryption keys
+	blockCipherReversed := BlockCipher(BlockCipher{})
+	blockCipherReversed.decryptionKey = blockCipher.encryptionKey
+	blockCipherReversed.encryptionKey = blockCipher.decryptionKey
+
+	encryptedData, err := blockCipher.EncryptWithAESGCM(suite.plainTextData)
+	assert.Nil(suite.T(), err)
+
+	decryptedData, err := blockCipherReversed.DecryptWithAESGCM(encryptedData)
+	assert.Nil(suite.T(), err)
+
+	assert.Equal(suite.T(), suite.plainTextData, decryptedData)
+}
+
+func (suite *BlockCipherTestSuite) TestGetCipherTextKey() {
+	var blockCipher IBlockCipher = &BlockCipher{cipherTextKey: suite.cipherTextKey}
+	assert.Equal(suite.T(), suite.cipherTextKey, blockCipher.GetCipherTextKey())
+}
+
+func (suite *BlockCipherTestSuite) TestGetKMSKeyId() {
+	var blockCipher IBlockCipher = &BlockCipher{kmsKeyId: suite.kmsKeyId}
+	assert.Equal(suite.T(), suite.kmsKeyId, blockCipher.GetKMSKeyId())
+}
---
+++ agent/crypto/kmsservice.go
@@ -0,0 +1,83 @@
+// Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"). You may not
+// use this file except in compliance with the License. A copy of the
+// License is located at
+//
+// http://aws.amazon.com/apache2.0/
+//
+// or in the "license" file accompanying this file. This file is distributed
+// on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+// either express or implied. See the License for the specific language governing
+// permissions and limitations under the License.
+
+// crypto package provides methods to encrypt and decrypt data
+package crypto
+
+import (
+	"fmt"
+	"github.com/aws/amazon-ssm-agent/agent/appconfig"
+	"github.com/aws/amazon-ssm-agent/agent/log"
+	"github.com/aws/amazon-ssm-agent/agent/sdkutil"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/request"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/kms"
+	"github.com/aws/aws-sdk-go/service/kms/kmsiface"
+)
+
+// KMSKeySizeInBytes is the key size that is fetched from KMS. 64 bytes key is split into two halves.
+// First half 32 bytes key is used by agent for encryption and second half 32 bytes by clients like cli/console
+const KMSKeySizeInBytes int64 = 64
+
+type IKMSService interface {
+	GenerateDataKey(kmsKeyId string) (cipherTextKey []byte, plainTextKey []byte, err error)
+}
+
+type KMSService struct {
+	client kmsiface.KMSAPI
+}
+
+// NewKMSService creates a new KMSService instance
+func NewKMSService(log log.T) (kmsService *KMSService, err error) {
+	var (
+		awsConfig        *aws.Config
+		appConfig        appconfig.SsmagentConfig
+		kmsClientSession *session.Session
+		agentName        string
+		agentVersion     string
+	)
+	awsConfig = sdkutil.AwsConfig()
+	if appConfig, err = appconfig.Config(false); err != nil {
+		log.Warnf("Failed to load appconfig: %s. Using default config.", err)
+	} else if appConfig.Kms.Endpoint != "" {
+		awsConfig.Endpoint = &appConfig.Kms.Endpoint
+	}
+	agentName = appConfig.Agent.Name
+	agentVersion = appConfig.Agent.Version
+	if kmsClientSession, err = session.NewSession(awsConfig); err != nil {
+		return nil, fmt.Errorf("Error creating new aws sdk session: %s", err)
+	}
+	kmsClientSession.Handlers.Build.PushBack(request.MakeAddToUserAgentHandler(agentName, agentVersion))
+	kmsService = &KMSService{
+		client: kms.New(kmsClientSession),
+	}
+
+	return kmsService, nil
+}
+
+// GenerateDataKey gets cipher text and plain text keys from KMS service
+func (kmsService *KMSService) GenerateDataKey(kmsKeyId string) (cipherTextKey []byte, plainTextKey []byte, err error) {
+	kmsKeySize := KMSKeySizeInBytes
+	generateDataKeyInput := kms.GenerateDataKeyInput{
+		KeyId:         &kmsKeyId,
+		NumberOfBytes: &kmsKeySize,
+	}
+
+	var generateDataKeyOutput *kms.GenerateDataKeyOutput
+	if generateDataKeyOutput, err = kmsService.client.GenerateDataKey(&generateDataKeyInput); err != nil {
+		return nil, nil, fmt.Errorf("Error calling KMS GenerateDataKey API: %s", err)
+	}
+
+	return generateDataKeyOutput.CiphertextBlob, generateDataKeyOutput.Plaintext, nil
+}
