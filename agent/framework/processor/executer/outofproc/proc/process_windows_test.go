35fc11903764cf5fbe42bbb97c282bd5d97611f2
Vishnu Karthik Ravindran <rvivi@amazon.com>
2020-05-15 21:28:25 -0400

    Goimports fix for process windows test file
    cr https://code.amazon.com/reviews/CR-25530543

--- agent/framework/processor/executer/outofproc/proc/process_windows_test.go
+++ agent/framework/processor/executer/outofproc/proc/process_windows_test.go
@@ -23,7 +23,7 @@ import (
 	"time"
 
 	"github.com/aws/amazon-ssm-agent/agent/log"
-	"src/github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/assert"
 )
 
 //TODO add process start time
