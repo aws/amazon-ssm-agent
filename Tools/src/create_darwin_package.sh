40dc63b911a47d877d82b5d90f6d824ab22b4050
Thor Bjorgvinsson <sigurtho@amazon.com>
2021-08-02 15:15:41 +0000

    Adding darwin arm64 to makefile
    cr: https://code.amazon.com/reviews/CR-54600632

--- Tools/src/create_darwin_package.sh
+++ Tools/src/create_darwin_package.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env bash
 
-cp ${GO_SPACE}/Tools/src/update/darwin/install_tar.sh ${GO_SPACE}/bin/darwin_amd64/darwin/install.sh
-cp ${GO_SPACE}/Tools/src/update/darwin/uninstall.sh ${GO_SPACE}/bin/darwin_amd64/darwin/uninstall.sh
+ARCH=$1
 
-chmod 755 ${GO_SPACE}/bin/darwin_amd64/darwin/install.sh
-chmod 755 ${GO_SPACE}/bin/darwin_amd64/darwin/uninstall.sh
-chmod 755 ${GO_SPACE}/bin/darwin_amd64/updater
+cp ${GO_SPACE}/Tools/src/update/darwin/install_tar.sh ${GO_SPACE}/bin/darwin_$ARCH/darwin/install.sh
+cp ${GO_SPACE}/Tools/src/update/darwin/uninstall.sh ${GO_SPACE}/bin/darwin_$ARCH/darwin/uninstall.sh
 
-tar -zcvf ${GO_SPACE}/bin/updates/amazon-ssm-agent/`cat ${GO_SPACE}/VERSION`/amazon-ssm-agent-darwin-amd64.tar.gz  -C ${GO_SPACE}/bin/darwin_amd64/darwin/ amazon-ssm-agent.tar.gz install.sh uninstall.sh
+chmod 755 ${GO_SPACE}/bin/darwin_$ARCH/darwin/install.sh
+chmod 755 ${GO_SPACE}/bin/darwin_$ARCH/darwin/uninstall.sh
+chmod 755 ${GO_SPACE}/bin/darwin_$ARCH/updater
 
-tar -zcvf ${GO_SPACE}/bin/updates/amazon-ssm-agent-updater/`cat ${GO_SPACE}/VERSION`/amazon-ssm-agent-updater-darwin-amd64.tar.gz  -C ${GO_SPACE}/bin/darwin_amd64/ updater
+tar -zcvf ${GO_SPACE}/bin/updates/amazon-ssm-agent/`cat ${GO_SPACE}/VERSION`/amazon-ssm-agent-darwin-$ARCH.tar.gz  -C ${GO_SPACE}/bin/darwin_$ARCH/darwin/ amazon-ssm-agent.tar.gz install.sh uninstall.sh
 
-rm ${GO_SPACE}/bin/darwin_amd64/darwin/install.sh
-rm ${GO_SPACE}/bin/darwin_amd64/darwin/uninstall.sh
+tar -zcvf ${GO_SPACE}/bin/updates/amazon-ssm-agent-updater/`cat ${GO_SPACE}/VERSION`/amazon-ssm-agent-updater-darwin-$ARCH.tar.gz  -C ${GO_SPACE}/bin/darwin_$ARCH/ updater
+
+rm ${GO_SPACE}/bin/darwin_$ARCH/darwin/install.sh
+rm ${GO_SPACE}/bin/darwin_$ARCH/darwin/uninstall.sh
