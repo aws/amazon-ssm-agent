ac2d20d6eaddac70a59fce342979451d624b3c7d
Haoran Ren <rhaoran@amazon.com>
2024-09-10 20:13:15 +0000

    [SeamlessDomainJoin] Change SUSE Linux zypper commands to quiet mode
    SIM: https://i.amazon.com/SSM-86603
    cr: https://code.amazon.com/reviews/CR-148103851

--- agent/plugins/domainjoin/domainjoin_unix_script.go
+++ agent/plugins/domainjoin/domainjoin_unix_script.go
@@ -438,8 +438,8 @@ install_components() {
          fi
          LINUX_DISTRO='SUSE'
          check_for_write_protect zypper
-         sudo zypper update -y
-         sudo zypper -n install realmd adcli sssd sssd-tools sssd-ad samba-client krb5-client samba-winbind krb5-client bind-utils python3 openldap2-client NetworkManager
+         sudo zypper -q update -y
+         sudo zypper -n -q install realmd adcli sssd sssd-tools sssd-ad samba-client krb5-client samba-winbind krb5-client bind-utils python3 openldap2-client NetworkManager
          if [ $? -ne 0 ]; then
             return 1
          fi
